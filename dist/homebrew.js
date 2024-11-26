ae29bbd755be707320c048be439373d0485a73a1
Italo Borrelli <iborrelli@oceannetworks.ca>
2024-11-26 13:08:55 -0800

    Remove dist from tracking

--- dist/homebrew.js
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=homebrew.js.map
--- dist/index.js
+++
@@ -1,4 +0,0 @@
-import { mapToHardcodex } from './5e_tools_to_hardcodex_monsters';
-import homebrew from './homebrew.json' assert { type: 'json' };
-console.log(mapToHardcodex(homebrew.monster[0]));
-//# sourceMappingURL=index.js.map
