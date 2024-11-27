45187f471ec09cdb19f953aac30fb18b16b9d039
Italo Borrelli <iborrelli@oceannetworks.ca>
2024-11-27 15:57:08 -0800

    Add item and spell card pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,65 @@
 
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+
 export default function Home() {
   return (
-    <Button asChild>
-      <Link href="/monster">Monster Cards</Link>
-    </Button>
+    <div className="flex justify-center items-center h-screen mx-2">
+      <div className="grid grid-cols-3 gap-8 place-content-center">
+        <div>
+          <Card>
+            <CardHeader>
+              <CardTitle className="centre">Item Cards</CardTitle>
+              <CardDescription>
+                Create custom item cards for your campaign.
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <Button asChild>
+                <Link href="/items">Items</Link>
+              </Button>
+            </CardContent>
+          </Card>
+        </div>
+        <div>
+          <Card>
+            <CardHeader>
+              <CardTitle className="centre">Monster Cards</CardTitle>
+              <CardDescription>
+                Create custom monster cards for your campaign.
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <Button asChild>
+                <Link href="/monsters">Monsters</Link>
+              </Button>
+            </CardContent>
+          </Card>
+        </div>
+        <div>
+          <Card>
+            <CardHeader>
+              <CardTitle className="centre">Spell Cards</CardTitle>
+              <CardDescription>
+                Create custom spell cards for your campaign.
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <Button asChild>
+                <Link href="/spells">Spells</Link>
+              </Button>
+            </CardContent>
+          </Card>
+        </div>
+      </div>
+    </div>
   );
 }
--- src/lib/navigation.tsx
+++ src/lib/navigation.tsx
@@ -1,8 +1,13 @@
+'use client';
+
 import {
   NavigationMenu,
+  NavigationMenuContent,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
 } from '@/components/ui/navigation-menu';
 import { navigationMenuTriggerStyle } from '@/components/ui/navigation-menu';
 import Link from 'next/link';
@@ -17,9 +22,20 @@ const Navigation: React.FC = () => (
         </Link>
       </NavigationMenuItem>
       <NavigationMenuItem>
-        <Link href="/monster" className={navigationMenuTriggerStyle()}>
-          <NavigationMenuLink>Monster Cards</NavigationMenuLink>
-        </Link>
+        <NavigationMenuTrigger className={navigationMenuTriggerStyle()}>
+          Cards
+        </NavigationMenuTrigger>
+        <NavigationMenuContent>
+          <Link href="/items" className={navigationMenuTriggerStyle()}>
+            <NavigationMenuLink>Items</NavigationMenuLink>
+          </Link>
+          <Link href="/monsters" className={navigationMenuTriggerStyle()}>
+            <NavigationMenuLink>Spells</NavigationMenuLink>
+          </Link>
+          <Link href="/spells" className={navigationMenuTriggerStyle()}>
+            <NavigationMenuLink>Monsters</NavigationMenuLink>
+          </Link>
+        </NavigationMenuContent>
       </NavigationMenuItem>
     </NavigationMenuList>
   </NavigationMenu>
