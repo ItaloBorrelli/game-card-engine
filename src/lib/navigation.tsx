9b568da76308a8c0e863d826c6294c6618f005d1
Italo Borrelli <iborrelli@oceannetworks.ca>
2024-11-27 17:29:06 -0800

    Implement linting rules and reorganize

--- src/lib/navigation.tsx
+++ src/lib/navigation.tsx
@@ -7,7 +7,6 @@ import {
   NavigationMenuLink,
   NavigationMenuList,
   NavigationMenuTrigger,
-  NavigationMenuViewport,
 } from '@/components/ui/navigation-menu';
 import { navigationMenuTriggerStyle } from '@/components/ui/navigation-menu';
 import Link from 'next/link';
